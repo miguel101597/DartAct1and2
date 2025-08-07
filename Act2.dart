4702dbc98f80248231d0c6c2a46b9ce4f53e9f1c
miguel101597 <168495419+miguel101597@users.noreply.github.com>
2025-08-07 12:53:46 +0800

    Activity 2 : Understanding the Dart

---
+++ Act2.dart
@@ -0,0 +1,19 @@
+ACTIVITY 1
+
+void main() {
+  String greeting = 'Welcome';
+  String user = 'Alice';
+  print('$greeting,  $user!' );
+ }
+
+void main() {
+  String greeting = 'Welcome';
+  dynamic  user = 'Alice';
+  print('$greeting,  $user!' );
+  
+  user = 25;
+  print('$greeting,  $user!' );
+ }
+
+
+dynamic variables can store any type, we can change it svalue.
